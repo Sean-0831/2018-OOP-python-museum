dfefb0d832229af5d9fb77e9664914ecd5c3721f
changmin2508 <changmin2508@naver.com>
2018-11-28 21:02:20 +0900

    Update 단어 사전 파싱.py

--- 단어 사전 파싱.py
+++ 단어 사전 파싱.py
@@ -2,32 +2,29 @@ import requests
 import bs4
 from urllib import parse
 
-INFO = {
-    'id': 'USER_ID'
-}
-
 def get_html(url):
-    """
-    웹 사이트 주소를 입력 받아, html tag 를 읽어드려 반환한다.
-    :param url: parsing target web url
-    :return: html tag
-    """
+
     response = requests.get(url)
     response.raise_for_status()
 
     return response.text
 
-a = input()
+while True: # 입력 대기
+    a = input()
+    if a == 'break': # 검색 종료 명령
+        break
+    c = 'https://opendict.korean.go.kr/search/searchResult?focus_name=query&query='
+    c = c + parse.quote(a) + '&dicType=1&wordMatch=Y&searchType=&currentPage=1&cateCode=&fieldCode=&spCode=&divSearch=search&infoType=confirm&rowsperPage=10&sort=W&side_data=0%7C104529'
 
-c = 'https://opendict.korean.go.kr/search/searchResult?focus_name=query&query='
-c = c + parse.quote(a) + '&dicType=1&wordMatch=Y&searchType=&currentPage=1&cateCode=&fieldCode=&spCode=&divSearch=search&infoType=confirm&rowsperPage=10&sort=W&side_data=0%7C104529'
+    html = get_html(c)
+    soup = bs4.BeautifulSoup(html, 'html.parser')
 
-html = get_html(c)
-soup = bs4.BeautifulSoup(html, 'html.parser')
+    INFO_main = soup.select('div.search_result dd span')
 
-INFO_main = soup.select('div.search_result dd span')
+    if not INFO_main: # 사전에 없는 단어를 검색한 경우 존재하지 않음을 사용자에게 출력
+        print('없습니다.')
 
-for i in INFO_main:
-    title = i.getText().strip()
-    print("%s" %title)
-
+    else:
+        for i in INFO_main:
+            title = i.getText().strip()
+            print("%s" %title)
